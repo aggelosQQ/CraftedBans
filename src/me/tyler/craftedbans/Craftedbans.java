15ec1b111a537964d8ac25e95caf8917141d07bd
buildcarter8 <tylerhuff2002@me.com>
2014-07-21 18:20:30 -0400

    V1.1.2!

--- src/me/tyler/craftedbans/Craftedbans.java
+++ src/me/tyler/craftedbans/Craftedbans.java
@@ -16,31 +16,37 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 public class Craftedbans extends JavaPlugin{
-	public final Logger logger = Logger.getLogger("Minecraft");
+	public final Logger log = Logger.getLogger("Minecraft-Server");
 	  public static Craftedbans plugin;
+	  public static final String VERSION = ("1.1.2");
 	  public static final String NOPE_YOU_CANT = (ChatColor.RED + "No permissions!");
 	  public static final String Not_Found = (ChatColor.GOLD + "Player not found!");
 	  public void onDisable()
 	  {
-	    PluginDescriptionFile pdfFile = getDescription();
-	    logger.info(pdfFile.getName() + " Has Been Disabled!");
+		log.info("[CraftedSmart]: Has been Disabled!");
+	    log.info("[CraftedBans]: Has Been Disabled!");
 	  }
 	  public void onEnable() 
 	  {
-	    PluginDescriptionFile pdfFile = getDescription();
-	    logger.info(pdfFile.getName() + " Version " + pdfFile.getVersion() + " Has Been Enabled");
+	    log.info("[CraftedSmart]: V01 Has been enabled");
+	    log.info("[CraftedBans]: Version " + VERSION +  " Has Been Enabled");
 	  }
-	  public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+	  public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args, boolean SenderisConsole) {
 	    Player player = (Player) sender;
 	     if(commandLabel.equalsIgnoreCase("ban")) {
 	    	if(sender.hasPermission("craftedbans.ban"))
 	    	if (args.length == 0)
-	    		sender.sendMessage(ChatColor.RED + "Usage: /ban <player> <unban>");
+	    		sender.sendMessage(ChatColor.RED + "Usage: /ban <player> <reason>");
 	    	else if (args.length == 1) {
 	    		if (player.getServer().getPlayer(args[0]) != null) {
 			          Player targetPlayer = player.getServer().getPlayer(args[0]);
-			            Bukkit.broadcastMessage(ChatColor.GRAY + targetPlayer.getName() + " Has been banned by " + sender.getName()); 
-                        targetPlayer.kickPlayer(ChatColor.GOLD + "Banned by " + sender.getName());
+			          String reason = "You have been banned!";
+			          if (args.length >= 2)
+			          {
+			              reason = StringUtils.join(ArrayUtils.subarray(args, 1, args.length), " ");
+			          }
+			            Bukkit.broadcastMessage(ChatColor.GRAY + targetPlayer.getName() + " Has been banned by " + sender.getName() + " For: " + reason); 
+                        targetPlayer.kickPlayer(ChatColor.GOLD + "Banned by " + sender.getName() + " For: " + reason);
 		    	        targetPlayer.setBanned(true);
 	    		}else{
 	    			OfflinePlayer offlinep = getServer().getOfflinePlayer(args[0]);
@@ -48,11 +54,6 @@ public class Craftedbans extends JavaPlugin{
 	    			Bukkit.broadcastMessage(ChatColor.GRAY + offlinep.getName() + " Has been banned by " + sender.getName());
 	    		}
 	    	}
-	    	    if (args.length == 2) {
-	    	    	OfflinePlayer offlinep = getServer().getOfflinePlayer(args[0]);
-	    	    	offlinep.setBanned(false);
-	    	    	Bukkit.broadcastMessage(ChatColor.GRAY + offlinep.getName() + " Has been unbanned by " + sender.getName());
-	    	    }
 	     }
 	        if(commandLabel.equalsIgnoreCase("kick")) {
 	    	if(sender.hasPermission("craftedbans.kick"))
@@ -61,18 +62,47 @@ public class Craftedbans extends JavaPlugin{
 	    	else if (args.length == 1) {
 	    		if (player.getServer().getPlayer(args[0]) != null) {
 			          Player targetPlayer = player.getServer().getPlayer(args[0]);
-			            Bukkit.broadcastMessage(ChatColor.GRAY + targetPlayer.getName() + " Has been kicked by " + sender.getName()); 
-                        targetPlayer.kickPlayer(ChatColor.GOLD + "Disconnected by " + sender.getName());
+			          String reason = "You have been kicked!";
+			          if (args.length >= 2)
+			          {
+			              reason = StringUtils.join(ArrayUtils.subarray(args, 1, args.length), " ");
+			          } 
+			          Bukkit.broadcastMessage(ChatColor.GRAY + targetPlayer.getName() + " Has been kicked by " + sender.getName() + " For: " + reason); 
+                        targetPlayer.kickPlayer(ChatColor.GOLD + "Kicked by " + sender.getName() + " For: " + reason);
 	    		}else{
 	    			sender.sendMessage(Not_Found);
 	    		}
-	    			
-		    	     
+	      if(cmd.getName().equalsIgnoreCase("unban"))
+	      {
+	    	 OfflinePlayer offlinep = getServer().getOfflinePlayer(args[0]);
+	    	 if(offlinep.isBanned());
+	    	 Bukkit.broadcastMessage(ChatColor.GRAY + offlinep.getName() +" has been unbanned by: " + sender.getName());
+	      }else{
+	    	  sender.sendMessage(ChatColor.RED + "[CraftedBans]: That player is not banned!");
+	     if(cmd.getName().equalsIgnoreCase("warn"))
+	     {
+		    	if(sender.hasPermission("craftedbans.warn"))
+			    	if (args.length == 0)
+			    		sender.sendMessage(ChatColor.RED + "CraftedSmart command- Warn a player Usage: /warn <player> <reason>");
+			    	else if (args.length == 1) {
+			    		if (player.getServer().getPlayer(args[0]) != null) {
+					          Player targetPlayer = player.getServer().getPlayer(args[0]);
+					          String reason = "You have been warned!";
+					          if (args.length >= 2)
+					          {
+					              reason = StringUtils.join(ArrayUtils.subarray(args, 1, args.length), " ");
+					          } 
+					         Bukkit.broadcastMessage("[CraftedSmart]: " + targetPlayer.getName() + " has been warned by: " + sender.getName() + " For: " + reason);
+	     }
 }
 	    	}
 	     {
 		
 	  }
-		return true;
 }
+			return SenderisConsole;
+}
+}
+			return true;
+	  }
 }
