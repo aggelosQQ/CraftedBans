dbb3c9c72070d6a13740750f8ad9da430ee0427c
aggelosQQ <jouflis1@hotmail.com>
2014-07-28 02:49:20 +0300

    Update MySQLManager.java

--- src/me/tyler/craftedbans/MySQLManager.java
+++ src/me/tyler/craftedbans/MySQLManager.java
@@ -15,7 +15,8 @@ private MySQL db;
                 "root","pass");
         this.db.openConnection();
         Statement statement = this.db.getConnection().createStatement();
-        statement.executeUpdate("CREATE TABLE IF NOT EXISTS 'Craftedbanslog' ('Name' varchar(32), 'Uses' int)");
+        statement
+                .executeUpdate("CREATE TABLE IF NOT EXISTS `Craftedbanslog` (`Name` varchar(32), `Uses` int)");
         statement.close();
     }
     
@@ -23,4 +24,32 @@ private MySQL db;
         this.db.closeConnection();
     }
     
-}    
+    public int getUses(Player p) throws SQLException {
+        String pname = p.getDisplayName().toLowerCase();
+        if(!this.db.checkConnection())
+            this.db.openConnection();
+        Statement statement = this.db.getConnection().createStatement();
+        ResultSet rs = statement
+        .executeQuery("SELECT * FROM `Craftedbanslog` WHERE `Name`='"
+        +pname+"';");
+        if(!rs.next())
+        return 0;
+    return rs getInt("Uses");
+    }
+    
+    public void updatePlayer(Player p) throws SQLException
+        String pname = p.getDisplayName().toLowerCase();
+        if(!this.db.checkConnection())
+            this.db.openConnection();
+        Statement statement = this.db.getConnection().createStatement();
+        int uses = this.getUses(p);
+        if(uses!=0){
+            statement.executeUpdate("UPDATE `Craftedbans` SET `Uses`='"+(uses+1)+' WHERE `Name`='"
+                        +pname+"';");
+        }else{
+            statement.executeUpdate("INSERT INTO `Craftedbanslog` (`Name`,`Uses`) VALUES'"
+                        + pname +"', '1');");
+            }
+        }
+    
+    }    
